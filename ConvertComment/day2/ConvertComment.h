7f7b05380b0735f3401b4a30cee35cee2b6fb579
zhangzhuo233 <1148581966@qq.com>
2017-02-10 15:55:32 +0800

    git add --all

--- ConvertComment/day2/ConvertComment.h
+++ ConvertComment/day2/ConvertComment.h
@@ -4,7 +4,7 @@
 #include"utili.h"
 
 extern "C" int ConvertComment(FILE *inputfile, FILE *outputfile);
-//å°†æ­¤å‡½æ•°ä»¥Cè¿›è¡Œç¼–è¯‘ï¼Œmain.cppå’Œconvertcomment.cppå¼ºåˆ¶ç¼–è¯‘å™¨ä¸è¦ä¿®æ”¹ä½ çš„å‡½æ•°åï¼Œè°ƒç”¨çš„æ˜¯åŒä¸€ä¸ªå‡½æ•°
+//½«´Ëº¯ÊıÒÔC½øĞĞ±àÒë£¬main.cppºÍconvertcomment.cppÇ¿ÖÆ±àÒëÆ÷²»ÒªĞŞ¸ÄÄãµÄº¯ÊıÃû£¬µ÷ÓÃµÄÊÇÍ¬Ò»¸öº¯Êı
 typedef enum
 {
     NO_COMMENT_STATE,
@@ -19,20 +19,20 @@ typedef struct
     FILE *inputfile;
     FILE *outputfile;
     STATE_ENUM state;
-    STATE_ENUM prestate;        //è®°å½•å…ˆå‰çš„çŠ¶æ€,è¿›å…¥å­—ç¬¦ä¸²å‰è¿›è¡Œè®°å½•
+    STATE_ENUM prestate;        //¼ÇÂ¼ÏÈÇ°µÄ×´Ì¬,½øÈë×Ö·û´®Ç°½øĞĞ¼ÇÂ¼
 }State_Machine;
 
 ///////////////////////////////////////////////////////////
 State_Machine  g_state;
 
-void EventPro(char ch);         //äº‹ä»¶å¤„ç†
-void EventProAtNo(char ch);     //æ— æ³¨é‡ŠçŠ¶æ€
-void EventProAtC(char ch);      //Cè¯­è¨€çŠ¶æ€
-void EventProAtCpp(char ch);    //CppçŠ¶æ€
-void EventProAtStr(char ch);    //å­—ç¬¦ä¸²çŠ¶æ€
-char readchr();                 //è¯»å–ä¸€ä¸ªå­—ç¬¦ä¸²
-void Write_One_Data(char ch);        //ç”¨æ¥å†™å…¥ä¸€ä¸ªå­—ç¬¦
-void Write_Data(char ch1, char ch2);//å†™å…¥ä¸¤ä¸ªå­—ç¬¦å‡½æ•°
+void EventPro(char ch);         //ÊÂ¼ş´¦Àí
+void EventProAtNo(char ch);     //ÎŞ×¢ÊÍ×´Ì¬
+void EventProAtC(char ch);      //CÓïÑÔ×´Ì¬
+void EventProAtCpp(char ch);    //Cpp×´Ì¬
+void EventProAtStr(char ch);    //×Ö·û´®×´Ì¬
+char readchr();                 //¶ÁÈ¡Ò»¸ö×Ö·û´®
+void Write_One_Data(char ch);        //ÓÃÀ´Ğ´ÈëÒ»¸ö×Ö·û
+void Write_Data(char ch1, char ch2);//Ğ´ÈëÁ½¸ö×Ö·ûº¯Êı
 
 ///////////////////////////////////////////////////////////
 int ConvertComment(FILE *inputfile, FILE *outputfile)
@@ -63,20 +63,20 @@ void EventPro(char ch)
 {
     switch(g_state.state)
     {
-    case NO_COMMENT_STATE:
-        EventProAtNo(ch);
-        break;
-    case C_COMMENT_STATE:
-        EventProAtC(ch);
-        break;
-    case CPP_COMMENT_STATE:
-        EventProAtCpp(ch);
-        break;
-    case STR_COMMENT_STATE:
-        EventProAtStr(ch);
-        break;
-    default:
-        break;
+        case NO_COMMENT_STATE:
+            EventProAtNo(ch);
+            break;
+        case C_COMMENT_STATE:
+            EventProAtC(ch);
+            break;
+        case CPP_COMMENT_STATE:
+            EventProAtCpp(ch);
+            break;
+        case STR_COMMENT_STATE:
+            EventProAtStr(ch);
+            break;
+        default:
+            break;
     }
 }
 
@@ -85,42 +85,42 @@ void EventProAtNo(char ch)
     char nextch;
     switch(ch)
     {
-    case '\n':
+        case '\n':
             Write_One_Data('\n');
             break;
-    case '/':
-        nextch = readchr();
-        if(nextch == '/')   //  //
-        {
-            Write_Data(ch, '*');
-            g_state.state = CPP_COMMENT_STATE;
-        }
-        else if(nextch == '*') // /*
-        {
-            Write_Data(ch, nextch);
-            g_state.state = C_COMMENT_STATE;
-        }
-        else
-            Write_Data(ch, nextch);
-        break;
-    case '*':
-        nextch = readchr();
-        if(nextch == '/')
-            Write_Data(' ', ' ');
-        else
-            Write_Data(ch, nextch);
-        break;
-    case '\"':
-        Write_One_Data(ch);
-        g_state.state = STR_COMMENT_STATE;
-        g_state.prestate = NO_COMMENT_STATE;
-        break;
-    case EOF:
-        g_state.state = END_COMMENT_STATE;
-        break;
-    default:
-        Write_One_Data(ch);
-        break;
+        case '/':
+            nextch = readchr();
+            if(nextch == '/')   //  //
+            {
+                Write_Data(ch, '*');
+                g_state.state = CPP_COMMENT_STATE;
+            }
+            else if(nextch == '*') // /*
+            {
+                Write_Data(ch, nextch);
+                g_state.state = C_COMMENT_STATE;
+            }
+            else
+                Write_Data(ch, nextch);
+            break;
+//        case '*':
+//            nextch = readchr();
+//            if(nextch == '/')
+//                Write_Data(' ', ' ');
+//            else
+//                Write_Data(ch, nextch);
+//            break;
+        case '\"':
+            Write_One_Data(ch);
+            g_state.state = STR_COMMENT_STATE;
+            g_state.prestate = NO_COMMENT_STATE;
+            break;
+        case EOF:
+            g_state.state = END_COMMENT_STATE;
+            break;
+        default:
+            Write_One_Data(ch);
+            break;
     }
 }
 void EventProAtC(char ch)
@@ -128,53 +128,53 @@ void EventProAtC(char ch)
     char nextch;
     switch(ch)
     {
-    case '*':
-        nextch = readchr();
-        if(nextch == '/')
-        {
-            Write_Data('*', '/');
-            g_state.state = NO_COMMENT_STATE;
-            g_state.prestate = NO_COMMENT_STATE;
-        }
-        else if(nextch == '*')
-        {
-            Write_One_Data(ch);
-            fseek(g_state.inputfile, -1L, SEEK_CUR);//å¥‡æ•°ä¸ª*ï¼Œæœ€åä¸€ä¸ªè®¾ç½®æˆä¸/åŒ¹é…çš„æƒ…å†µ,eg./***/
-        }
-        else
-            Write_Data(ch, nextch);
-            break;
-    case '\n':
-        if(g_state.prestate == CPP_COMMENT_STATE)
-        {
-            Write_Data('*', '/');
-            g_state.state = NO_COMMENT_STATE;
-        }
-        else
-            Write_One_Data('\n');
-        break;
-    case '\"':
-        Write_One_Data(ch);
-        g_state.state = STR_COMMENT_STATE;
-        g_state.prestate = C_COMMENT_STATE;
-        break;
-    case '/':
-        nextch = readchr();
-        if(nextch == '/')
-        {
-            Write_Data(' ', ' ');
-        }
-        else
-        {
+        case '*':
+            nextch = readchr();
+            if(nextch == '/')
+            {
+                Write_Data('*', '/');
+                g_state.state = NO_COMMENT_STATE;
+                g_state.prestate = NO_COMMENT_STATE;
+            }
+            else if(nextch == '*')
+            {
+                Write_One_Data(ch);
+                fseek(g_state.inputfile, -1L, SEEK_CUR);//ÆæÊı¸ö*£¬×îºóÒ»¸öÉèÖÃ³ÉÓë/Æ¥ÅäµÄÇé¿ö,eg./***/
+            }
+            else
                 Write_Data(ch, nextch);
-        }
-        break;
-    case EOF:
-        g_state.state = END_COMMENT_STATE;
-        break;
-    default:
-        Write_One_Data(ch);
-        break;
+            break;
+        case '\n':
+            if(g_state.prestate == CPP_COMMENT_STATE)
+            {
+                Write_Data('*', '/');
+                g_state.state = NO_COMMENT_STATE;
+            }
+            else
+                Write_One_Data('\n');
+            break;
+        case '\"':
+            Write_One_Data(ch);
+            g_state.state = STR_COMMENT_STATE;
+            g_state.prestate = C_COMMENT_STATE;
+            break;
+        case '/':
+            nextch = readchr();
+            if(nextch == '/')
+            {
+                Write_Data(' ', ' ');
+            }
+            else
+            {
+                Write_Data(ch, nextch);
+            }
+            break;
+        case EOF:
+            g_state.state = END_COMMENT_STATE;
+            break;
+        default:
+            Write_One_Data(ch);
+            break;
     }
 }
 void EventProAtCpp(char ch)
@@ -182,64 +182,64 @@ void EventProAtCpp(char ch)
     char nextch;
     switch(ch)
     {
-    case '\n':
-        Write_Data('*', '/');
+        case '\n':
+            Write_Data('*', '/');
             Write_One_Data('\n');
             g_state.state = NO_COMMENT_STATE;
             g_state.prestate = NO_COMMENT_STATE;
-        break;
-    case '\"':
-        Write_One_Data(ch);
-        g_state.state = STR_COMMENT_STATE;
-        g_state.prestate = CPP_COMMENT_STATE;
-        break;
-    case '/':
-        nextch = readchr();
-        if(nextch == '/')
-        {
-            Write_Data(' ', ' ');
-        }
-        else if(nextch == '*')
-        {
-            Write_Data(' ', ' ');
-        }
-        else
-        {
-            fseek(g_state.inputfile, -3L, SEEK_CUR);
-            char bak_3 = readchr();
-            if(bak_3 == '/')
-                Write_One_Data(' ');
-            else
-                Write_One_Data(ch);
-            fseek(g_state.inputfile, +1L, SEEK_CUR);
-        }
-        g_state.prestate = CPP_COMMENT_STATE;
-        break;
-    case '*':
-        nextch = readchr();
-        if(nextch == '/')
-        {
-            if(g_state.prestate == CPP_COMMENT_STATE)
+            break;
+        case '\"':
+            Write_One_Data(ch);
+            g_state.state = STR_COMMENT_STATE;
+            g_state.prestate = CPP_COMMENT_STATE;
+            break;
+        case '/':
+            nextch = readchr();
+            if(nextch == '/')
+            {
+                Write_Data(' ', ' ');
+            }
+            else if(nextch == '*')
             {
                 Write_Data(' ', ' ');
             }
             else
             {
-                Write_Data(ch, nextch);
-                g_state.state = NO_COMMENT_STATE;
-                g_state.prestate = CPP_COMMENT_STATE;
+                fseek(g_state.inputfile, -3L, SEEK_CUR);
+                char bak_3 = readchr();
+                if(bak_3 == '/')
+                    Write_One_Data(' ');
+                else
+                    Write_One_Data(ch);
+                fseek(g_state.inputfile, +1L, SEEK_CUR);
             }
-        }
-        else
-            Write_Data(ch, nextch);
-        break;
-    case EOF:
-        Write_Data('*', '/');
-        g_state.state = END_COMMENT_STATE;
-        break;
-    default:
-        Write_One_Data(ch);
-        break;
+            g_state.prestate = CPP_COMMENT_STATE;
+            break;
+        case '*':
+            nextch = readchr();
+            if(nextch == '/')
+            {
+                if(g_state.prestate == CPP_COMMENT_STATE)
+                {
+                    Write_Data(' ', ' ');
+                }
+                else
+                {
+                    Write_Data(ch, nextch);
+                    g_state.state = NO_COMMENT_STATE;
+                    g_state.prestate = CPP_COMMENT_STATE;
+                }
+            }
+            else
+                Write_Data(ch, nextch);
+            break;
+        case EOF:
+            Write_Data('*', '/');
+            g_state.state = END_COMMENT_STATE;
+            break;
+        default:
+            Write_One_Data(ch);
+            break;
     }
 }
 void EventProAtStr(char ch)
@@ -252,12 +252,17 @@ void EventProAtStr(char ch)
             g_state.state = g_state.prestate;
             break;
         case '*':
-            nextch = fgetc(g_state.inputfile);
+            nextch = readchr();
             if(g_state.prestate == CPP_COMMENT_STATE && nextch == '/') // "abc*/"
+                Write_Data('#','#');
+            else if(g_state.prestate == C_COMMENT_STATE && nextch == '/')//  /*"ab*/"
             {
-                fputc('@', g_state.outputfile);
-                fputc('@', g_state.outputfile);
+                Write_Data(ch,nextch);
+                g_state.state = NO_COMMENT_STATE;
+                g_state.prestate = NO_COMMENT_STATE;
             }
+            else
+                Write_Data(ch, nextch);
             break;
         case EOF:
             g_state.state = END_COMMENT_STATE;
@@ -269,8 +274,8 @@ void EventProAtStr(char ch)
 }
 char readchr()
 {
-     ch = fgetc(g_state.inputfile);
-     return ch;
+    char ch = fgetc(g_state.inputfile);
+    return ch;
 }
 void Write_One_Data(char ch)
 {
@@ -281,5 +286,4 @@ void Write_Data(char ch1, char ch2)
     fputc(ch1, g_state.outputfile);
     fputc(ch2, g_state.outputfile);
 }
-
 #endif
